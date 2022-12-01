3279fcccc624b45078a5be17c9dae2bb6b5fa5bf
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-12-01 11:13:35 +0100

    First day solution

---
+++ Day_1_Calorie_Counting/day_1_file.py
@@ -0,0 +1,41 @@
+def load_puzzle_input() -> list:
+    with open("Day_1_Calorie_Counting/puzzle_input.txt") as f:
+        calorie_list = f.readlines()
+    return calorie_list
+
+def split_lines_to_elves(calorie_list: list) -> dict:
+    elf_pack = {0: {"calories": [], "total": []}}
+    elf_index = 0
+    for calorie_item in calorie_list:
+        if calorie_item != "\n":
+            elf_pack[elf_index]["calories"].append(int(calorie_item.strip("\n")))
+        else:
+            elf_pack[elf_index]["total"] = sum(elf_pack[elf_index]["calories"])
+            elf_index += 1
+            elf_pack[elf_index] = {"calories": [], "total": []}
+
+    # Making sure we get the sum for the last elf too
+    elf_pack[elf_index]["total"] = sum(elf_pack[elf_index]["calories"])
+
+    return elf_pack
+
+def get_N_elves_with_largest_packs(N: int, elf_pack: dict) -> None:
+    total_calories_per_pack = []
+    for elf_calories in elf_pack.values():
+        total_calories_per_pack.append(elf_calories["total"])
+
+    total_calories_per_pack.sort()
+
+    print(f"The elves with the {N} highest calories carry:\n")
+    for calories in total_calories_per_pack[-N:]:
+        print(f"\t{calories} calories \n")
+    print(f"In total, these {N} elves carry {sum(total_calories_per_pack[-N:])} calories")
+
+
+if __name__ == "__main__":
+    calorie_list = load_puzzle_input()
+
+    elf_pack = split_lines_to_elves(calorie_list)
+
+    get_N_elves_with_largest_packs(1, elf_pack)
+    get_N_elves_with_largest_packs(3, elf_pack)
