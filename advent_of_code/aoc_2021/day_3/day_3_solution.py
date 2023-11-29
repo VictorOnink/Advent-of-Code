617eb45e931e89d9b19946f00b41e9cc2ef42613
Victor Onink <victor.onink@nl.abnamro.com>
2023-11-29 21:45:51 +0100

    formatting

--- advent_of_code/aoc_2021/day_3/day_3_solution.py
+++ advent_of_code/aoc_2021/day_3/day_3_solution.py
@@ -1,15 +1,17 @@
+from copy import deepcopy
+
 from advent_of_code.utils.data_loader import Day, PuzzleCase, Year, load_puzzle_input
 from advent_of_code.utils.objects import PuzzleInput
 
-from copy import deepcopy
 
 def get_most_common_bit_diagnostic(diagnostic: PuzzleInput, number_of_bits: int) -> str:
     mcb_diagnostic = ""
-    
+
     for bit_num in range(number_of_bits):
         mcb_diagnostic += str(most_common_bit(diagnostic, bit_num))
 
     return mcb_diagnostic
+
 
 def most_common_bit(diagnostic: PuzzleInput, bit_index: int) -> int:
     number_rows_diagnostic = len(diagnostic)
@@ -17,12 +19,14 @@ def most_common_bit(diagnostic: PuzzleInput, bit_index: int) -> int:
     equal_to_0 = number_rows_diagnostic - equal_to_1
     return int(equal_to_1 >= equal_to_0)
 
+
 def invert_bits(bit_string: str) -> str:
     map_dict = {"1": "0", "0": "1"}
 
     invert: str = "".join(map_dict[bit] for bit in bit_string)
 
     return invert
+
 
 def binary_to_decimal(bit_string: str) -> int:
     number_of_bits = len(bit_string)
@@ -33,33 +37,40 @@ def binary_to_decimal(bit_string: str) -> int:
 
     return decimal
 
+
 def get_oxygen_generator_rating(diagnostic: PuzzleInput) -> str:
     diagnostic_selector = deepcopy(diagnostic)
-    
+
     bit_index = 0
 
     while len(diagnostic_selector) > 1:
         mcb = most_common_bit(diagnostic_selector, bit_index)
 
-        diagnostic_selector = [value for value in diagnostic_selector if value[bit_index] == str(mcb)]
+        diagnostic_selector = [
+            value for value in diagnostic_selector if value[bit_index] == str(mcb)
+        ]
 
         bit_index += 1
 
     return diagnostic_selector[0]
 
+
 def get_co2_scrubber_rating(diagnostic: PuzzleInput) -> str:
     diagnostic_selector = deepcopy(diagnostic)
-    
+
     bit_index = 0
 
     while len(diagnostic_selector) > 1:
         lcb = invert_bits(str(most_common_bit(diagnostic_selector, bit_index)))
 
-        diagnostic_selector = [value for value in diagnostic_selector if value[bit_index] == lcb]
+        diagnostic_selector = [
+            value for value in diagnostic_selector if value[bit_index] == lcb
+        ]
 
         bit_index += 1
 
     return diagnostic_selector[0]
+
 
 def solution(case: str) -> None:
     diagnostic: PuzzleInput = load_puzzle_input(Year(2021), Day(3), PuzzleCase(case))
