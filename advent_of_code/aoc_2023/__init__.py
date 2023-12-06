bd628afa74db4ecfcd734a2a339d51700d6d48fc
Victor Onink <victor.onink@nl.abnamro.com>
2023-12-06 10:51:41 +0100

    solution aoc 2023 day 6

--- advent_of_code/aoc_2023/__init__.py
+++ advent_of_code/aoc_2023/__init__.py
@@ -4,6 +4,7 @@ from .day_3.day_3_solution import solution as solution_day_3
 from .day_4.day_4_solution import solution as solution_day_4
 from .day_5.day_5_solution import solution as solution_day_5
 from .day_6.day_6_solution import solution as solution_day_6
+from .day_7.day_7_solution import solution as solution_day_7
 
 SOLUTIONS_2023 = {
     1: solution_day_1,
@@ -11,5 +12,6 @@ SOLUTIONS_2023 = {
     3: solution_day_3,
     4: solution_day_4,
     5: solution_day_5,
-    6: solution_day_6
+    6: solution_day_6,
+    7: solution_day_7,
 }
--- advent_of_code/aoc_2023/day_6/day_6_solution.py
+++ advent_of_code/aoc_2023/day_6/day_6_solution.py
@@ -1,8 +1,46 @@
+from collections import defaultdict
+from typing import Dict, List
+
 from advent_of_code.utils.data_loader import Day, PuzzleCase, Year, load_puzzle_input
 from advent_of_code.utils.objects import PuzzleInput
 
 
+def get_winning_ways(
+    times: List[int], distance: List[int], charge_speed: int = 1
+) -> Dict[int, int]:
+    winning_combos = defaultdict(int)
+
+    for race_id in range(len(times)):
+        for charge_time in range(times[race_id]):
+            v_boat = charge_time * charge_speed
+            remaining_time = times[race_id] - charge_time
+            if distance[race_id] < v_boat * remaining_time:
+                winning_combos[race_id] += 1
+
+    return winning_combos
 
 
 def solution(case: str) -> None:
     raw_input: PuzzleInput = load_puzzle_input(Year(2023), Day(6), PuzzleCase(case))
+
+    times: List[int] = [int(t) for t in raw_input[0].split(":")[1].split(" ") if t]
+    distance: List[int] = [int(x) for x in raw_input[1].split(":")[1].split(" ") if x]
+
+    winning_combos = get_winning_ways(times, distance)
+
+    multiple_ways = 1
+    for combos in winning_combos.values():
+        multiple_ways *= combos
+
+    print(f"The product of ways to win the race is {multiple_ways}")
+
+    times_p2: List[int] = [int(raw_input[0].split(":")[1].replace(" ", ""))]
+    distance_p2: List[int] = [int(raw_input[1].split(":")[1].replace(" ", ""))]
+
+    winning_combos_p2 = get_winning_ways(times_p2, distance_p2)
+
+    multiple_ways_p2 = 1
+    for combos in winning_combos_p2.values():
+        multiple_ways_p2 *= combos
+
+    print(f"The product of ways to win the race is {multiple_ways_p2}")
